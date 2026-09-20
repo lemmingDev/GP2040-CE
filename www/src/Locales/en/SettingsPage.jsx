d3f8d557e242eb83249e77dfb770aedb32ac7dae
lemmingDev <jadewilliams77@hotmail.com>
2026-09-20 10:01:57 +1000

    Add Bluetooth web configuration labels

--- www/src/Locales/en/SettingsPage.jsx
+++ www/src/Locales/en/SettingsPage.jsx
@@ -26,6 +26,8 @@ export default {
 		xboxoriginal: 'Original Xbox',
 		xbone: 'Xbox One',
 		sinput: 'SInput',
+		switch-bluetooth: 'Nintendo Switch Bluetooth',
+		hid-bluetooth: 'Generic HID Bluetooth',
 	},
 	'input-mode-group': {
 		primary: 'Primary Input Modes',
