7b48b604b69b49175e2cc57151ae6c7aaa6cb09c
lemmingDev <jadewilliams77@hotmail.com>
2026-09-26 21:21:24 +1000

    gp-link: wider trigger bar markers

--- www/src/Addons/GPLinkAnalog.tsx
+++ www/src/Addons/GPLinkAnalog.tsx
@@ -467,20 +467,20 @@ const TriggerBar = ({
 			<div
 				style={{
 					position: 'absolute',
-					left: `calc(${pct(v)}% - 1px)`,
+					left: `calc(${pct(v)}% - 2px)`,
 					top: 0,
 					bottom: 0,
-					width: 2,
+					width: 4,
 					background: 'var(--bs-primary)',
 				}}
 			/>
 			<div
 				style={{
 					position: 'absolute',
-					left: `calc(${Math.min(100, Math.max(0, (s / 255) * 100))}% - 1px)`,
+					left: `calc(${Math.min(100, Math.max(0, (s / 255) * 100))}% - 2px)`,
 					top: 0,
 					bottom: 0,
-					width: 2,
+					width: 4,
 					background: 'var(--bs-success)',
 				}}
 			/>
